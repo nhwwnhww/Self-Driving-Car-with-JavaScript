d5afbdf023483202062cd3ce8fcf1592252c6b9c
nhwwnhww <nhwenhww@gmail.com>
2022-05-16 15:43:21 +1000

    main

---
+++ controls.js
@@ -0,0 +1,45 @@
+class Controls{
+    constructor(){
+        this.foward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+    
+        this.#addKeyboardListeners();
+    }
+
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.foward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.foward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+            }
+        }
+    }
+}
